6a7b482db93fd598921f63ee81ff949ef4ef0c30
xianweili <lixw1021@gmail.com>
2018-05-12 17:21:25 -0700

    delete commit

--- app/src/main/java/com/xianwei/gittest/MainActivity.java
+++ app/src/main/java/com/xianwei/gittest/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // this is added from master branch
+
+        // previous commit has been delted
     }
 }
